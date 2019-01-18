21eb35530aaffad28fc4c2e850c557a3729348db
Sam Bengtson <sambeng1@gmail.com>
2019-01-18 10:52:38 -0600

    Second commit, still setting everything up

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const main = require('./views/main')
+const layout = require('./views/layout')
+const { db } = require('./models')
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res, next) => {
+  res.send(layout(''))
+})
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+});
