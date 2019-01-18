232c7a10586af03d56e1240b3671556e50f285b4
Sam Bengtson <sambeng1@gmail.com>
2019-01-18 14:28:16 -0600

    After lunch commit 2

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,12 +1,37 @@
 const router = require("express").Router();
 const addPage = require("../views/addPage");
+const { Page } = require('../models')
+
 
 router.get("/", (req, res, next) => {
   res.send("test");
 });
 
-router.post("/", (req, res, next) => {
-  res.send("heeey post");
+router.post("/", async (req, res, next) => {
+  
+  let sluggified = req.body.title
+  let newSlug = ''
+
+  const sluggify = (sluggified) => {
+    let regex = / /gi;
+    console.log(sluggified.replace(regex, '_'))
+    newSlug = sluggified.replace(regex, '_')
+  }
+
+  sluggify(sluggified)
+
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content,
+    slug: newSlug
+  })
+
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error){
+    { next(error) }
+  }
 });
 
 router.get("/add", (req, res, next) => {
@@ -14,3 +39,5 @@ router.get("/add", (req, res, next) => {
 });
 
 module.exports = router;
+
+
